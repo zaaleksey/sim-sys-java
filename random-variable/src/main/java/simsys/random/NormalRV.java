29a6559de1b67d05b3490e01b4b4d7e559f4e890
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-19 22:38:47 +0400

    normal rv fix

--- random-variable/src/main/java/simsys/random/NormalRV.java
+++ random-variable/src/main/java/simsys/random/NormalRV.java
@@ -16,7 +16,16 @@ public class NormalRV implements RandomVariable{
 
   @Override
   public double nextValue() {
-    return random.nextGaussian() * this.mean + this.sigma;
+    return this.mean + this.sigma * random.nextGaussian();
+  }
+
+  //  TODO: don't forget to clean up
+  public static void main(String[] args) {
+    RandomVariable rv = new NormalRV(new Random(), 5, 1);
+
+    for(int i = 0; i < 100; i++) {
+      System.out.println(rv.nextValue());
+    }
   }
 
 }
