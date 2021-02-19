49aedb5aabf05d12030b120aafd182222c5e444c
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-19 18:42:13 +0400

    normal rv

--- random-variable/src/main/java/simsys/random/NormalRV.java
+++ random-variable/src/main/java/simsys/random/NormalRV.java
@@ -1,10 +1,22 @@
 package simsys.random;
 
+import java.util.Random;
+
 public class NormalRV implements RandomVariable{
+
+  private final Random random;
+  private final double mean;
+  private final double sigma;
+
+  public NormalRV(Random random, double mean, double sigma) {
+    this.random = random;
+    this.mean = mean;
+    this.sigma = sigma;
+  }
 
   @Override
   public double nextValue() {
-    return 0;
+    return random.nextGaussian() * this.mean + this.sigma;
   }
 
 }
