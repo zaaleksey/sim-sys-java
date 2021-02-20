ce2b39aef7afb6660696d5181d4929ad16bf6e31
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-20 11:31:21 +0400

    erlang rv

--- random-variable/src/main/java/simsys/random/ErlangRV.java
+++ random-variable/src/main/java/simsys/random/ErlangRV.java
@@ -1,10 +1,37 @@
 package simsys.random;
 
+import java.util.Random;
+
 public class ErlangRV implements RandomVariable {
+
+  private final Random random;
+  private final int n;
+  private final double rate;
+
+  public ErlangRV(Random random, int n, double rate) {
+    this.random = random;
+    this.n = n;
+    this.rate = rate;
+  }
 
   @Override
   public double nextValue() {
-    return 0;
+    return -Math.log(getProductUniform01(this.n)) / this.rate;
+  }
+
+  private double getProductUniform01(int n) {
+    double product = 1;
+    double[] arr = new double[n];
+
+    for(int i = 0; i < n; i++) {
+      arr[i] = this.random.nextDouble();
+    }
+
+    for (int i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+
+    return product;
   }
 
 }
