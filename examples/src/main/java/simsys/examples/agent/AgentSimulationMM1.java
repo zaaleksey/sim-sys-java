6bfc203fe6c5f9cfe1f08af27524550343c2e800
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-02-20 13:18:30 +0400

    agent simulation mm1

--- examples/src/main/java/simsys/examples/agent/AgentSimulationMM1.java
+++ examples/src/main/java/simsys/examples/agent/AgentSimulationMM1.java
@@ -2,6 +2,9 @@ package simsys.examples.agent;
 
 import simsys.core.agent.AbstractAgent;
 import simsys.core.agent.Agent;
+import simsys.core.condition.TimeStopCondition;
+import simsys.core.context.SimulationContextImpl;
+import simsys.core.model.AgentBasedSimulationModel;
 import simsys.random.ExponentialRV;
 import simsys.random.RandomVariable;
 
@@ -19,7 +22,17 @@ public class AgentSimulationMM1 {
           RandomVariable in = new ExponentialRV(new Random(), lambda);
           RandomVariable out = new ExponentialRV(new Random(), mu);
 
-          
+//          определить состяния
+
+//          функцию action для перехода из состояния в состояние
+
         };
+
+      AgentBasedSimulationModel agentSimulationMM1 = new AgentBasedSimulationModel(
+          SimulationContextImpl.getEmptyInstance());
+      agentSimulationMM1.setStopCondition(new TimeStopCondition(100));
+      agentSimulationMM1.addAgent(mm1Agent);
+      agentSimulationMM1.run();
+
     }
 }
