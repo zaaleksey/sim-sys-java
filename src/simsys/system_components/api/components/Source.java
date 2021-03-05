4ea40ae2ba96a6d90f25b4e78f60a0f07a75a7c4
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-03-05 17:22:49 +0400

    cleaning from old code

--- src/simsys/system_components/api/components/Source.java
+++
@@ -1,7 +0,0 @@
-package simsys.system_components.api.components;
-
-import simsys.system_components.api.node.Node;
-
-public abstract class Source extends Node{
-    abstract void sendRequest(Node recipient);
-}
