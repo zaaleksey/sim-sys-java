a96d421eb3775b8dd3bf609ab462ca5e7f3282af
Alexey Zavarzin <alex.zavr.27100@mail.ru>
2021-05-28 23:31:20 +0400

    slow server

--- examples/src/main/java/simsys/examples/agent/SlowServerProblem.java
+++ examples/src/main/java/simsys/examples/agent/SlowServerProblem.java
@@ -1,10 +1,5 @@
 package simsys.examples.agent;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Random;
 import simsys.component.source.SourceAgent;
 import simsys.component.system.QueueingSystemWithTwoServer;
 import simsys.core.condition.TimeStopCondition;
@@ -15,69 +10,38 @@ import simsys.entity.queue.Queue;
 import simsys.entity.queue.QueueFIFO;
 import simsys.random.ExponentialRandomVariable;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class SlowServerProblem {
 
-  public static void main(String[] args) {
-    SimulationContext context = SimulationContextImpl.getContext();
-    Random r = new Random();
-    double simulationDuration = 10_000_000;
-    double warmUpDuration = simulationDuration * 0;
+    public static void main(String[] args) {
+        SimulationContext context = SimulationContextImpl.getContext();
+        Random r = new Random();
+        double lambda = 3;
+        SourceAgent source = new SourceAgent(context,
+                new ExponentialRandomVariable(r, lambda),
+                "Source");
 
-    double lambda = 3;
-    SourceAgent source = new SourceAgent(context,
-        new ExponentialRandomVariable(r, lambda),
-        "Source");
+        int capacity = 10;
+        Queue queue = new QueueFIFO(capacity);
+        QueueingSystemWithTwoServer queueing = new QueueingSystemWithTwoServer(context,
+                queue, new ExponentialRandomVariable(r, 0), "QueueingSystemWithTwoServer");
 
-    double learningRate = 0.1;
-    double rewardDecay = 1;
-    double eGreedy = 0.4;
+        source.setReceiver(queueing);
 
-    int capacity = 10;
-    Queue queue = new QueueFIFO(capacity);
-    QueueingSystemWithTwoServer queueing =
-        new QueueingSystemWithTwoServer(context, queue, new ExponentialRandomVariable(r, 0),
-            "QueueingSystemWithTwoServer", learningRate, rewardDecay, eGreedy, warmUpDuration);
+        AgentBasedSimulationModel agentSimulationMM1 = new AgentBasedSimulationModel(context);
+        agentSimulationMM1.setStopCondition(new TimeStopCondition(10_000_000));
+        agentSimulationMM1.addAgents(Arrays.asList(source, queueing));
+        source.sendDemand();
 
-    source.setReceiver(queueing);
-    var model = new AgentBasedSimulationModel(context);
-    model.setStopCondition(new TimeStopCondition(simulationDuration));
-    model.addAgents(Arrays.asList(source, queueing));
-    source.sendDemand();
-
-    long startTime = System.nanoTime();
-    model.run();
-    long endTime = System.nanoTime();
-    long timeElapsed = endTime - startTime;
-    System.out.println("Elapsed time = " + (double) (timeElapsed) / 1_000_000_000);
-    System.out.println("Q-table:");
-    queueing.printQTable();
-
-    System.out.println("Sojourn TIme: " + queueing.getStatisticForVariable("SOJOURN_TIME").mean());
-
-    try {
-      FileWriter fileWriter = new FileWriter("q-learn.dat");
-      PrintWriter printWriter = new PrintWriter(fileWriter);
-      // Plot with some step,to reduce amount of points
-      int step = queueing.rewardRates.size() / 100_000;
-      for (int i = 0; i < queueing.rewardRates.size(); i += step) {
-        printWriter.println(queueing.rewardRates.get(i));
-      }
-      fileWriter.close();
-    } catch (IOException e) {
-      e.printStackTrace();
+        long startTime = System.nanoTime();
+        agentSimulationMM1.run();
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Elapsed time = " + (double) (timeElapsed) / 1_000_000_000);
+        System.out.println("Average service time on a FAST server is " + queueing.getServerStatistic("SOJOURN_TIME").get(0).mean());
+        System.out.println("Average service time on a SLOW server is " + queueing.getServerStatistic("SOJOURN_TIME").get(1).mean());
     }
-
-    System.out.println("Policy");
-    var policy = queueing.getPolicy();
-    System.out.println(policy);
-
-    System.out.println("Completed");
-
-//    System.out.println("Average service time on a FAST server is "
-//        + queueing.getServerStatistic("SOJOURN_TIME").get(0).mean());
-//
-//    System.out.println("Average service time on a SLOW server is "
-//        + queueing.getServerStatistic("SOJOURN_TIME").get(1).mean());
-  }
 
 }
---
+++ queueing-component-library/src/main/java/simsys/component/system/QueueingSystemWithTwoServerQL.java
@@ -0,0 +1,249 @@
+package simsys.component.system;
+
+import org.apache.commons.lang3.ArrayUtils;
+import simsys.core.context.SimulationContext;
+import simsys.core.parcel.Parcel;
+import simsys.entity.demand.Demand;
+import simsys.entity.queue.Queue;
+import simsys.random.ExponentialRandomVariable;
+import simsys.random.RandomVariable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class QueueingSystemWithTwoServerQL extends AbstractQueueingSystem {
+
+  private static final double FAST_SERVER_RATE = 1;
+  private static final double SLOW_SERVER_RATE = 0.9;
+
+  private static final String SOJOURN_TIME = "SOJOURN_TIME";
+
+
+  private Demand fastServerDemand;
+  private Demand slowServerDemand;
+
+
+  private RandomVariable slowServerRV;
+  private RandomVariable fastServerRV;
+
+
+  private double[][] qTable;
+  private final int stateNumber;
+
+
+  private final int ACTION_NUMBER = 2;
+
+  private double alpha;
+  private double gamma;
+  private double epsilon;
+
+
+  private final Random r;
+
+
+  private double accumulatedRewardBetweenMakingDecisions;
+  private int lastAction;
+  private int stateBeforeLastAction;
+  private double previousTimeForPerformActionCall;
+  private double totalReward;
+  public ArrayList<Double> rewardRates;
+  private double warmUpDuration;
+  private double lastTimeDecision = 0;
+
+  public QueueingSystemWithTwoServerQL(SimulationContext context, Queue queue,
+                                       RandomVariable randomVariable, String agentName,
+                                       double learningRate, double rewardDecay, double eGreedy, double warmUpDuration) {
+    super(context, queue, randomVariable, agentName);
+
+    r = new Random();
+
+    this.alpha = learningRate;
+    this.gamma = rewardDecay;
+    this.epsilon = eGreedy;
+    this.warmUpDuration = warmUpDuration;
+
+    this.stateNumber = queue.capacity() + 1;
+
+    this.slowServerRV = new ExponentialRandomVariable(r, SLOW_SERVER_RATE);
+    this.fastServerRV = new ExponentialRandomVariable(r, FAST_SERVER_RATE);
+
+    accumulatedRewardBetweenMakingDecisions = 0;
+    lastAction = 1;
+    stateBeforeLastAction = 1;
+    previousTimeForPerformActionCall = 0;
+    totalReward = 0;
+    rewardRates = new ArrayList<>();
+    initQTable();
+  }
+
+
+  public double getRewardForCurrentState() {
+    int q = getNumberDemandInSystem();
+    return -q;
+  }
+
+  public int getCurrentState() {
+    return queue.size();
+  }
+
+  public void trackRewardRate(double rewardRate) {
+    rewardRates.add(rewardRate);
+  }
+
+  public void updateReward() {
+    double delta = context.getCurrentTime() - previousTimeForPerformActionCall;
+    accumulatedRewardBetweenMakingDecisions += getRewardForCurrentState() * delta;
+    previousTimeForPerformActionCall = context.getCurrentTime();
+  }
+
+
+  // печально, но параметры пока в performAction передать нельзя,  поэтому нужно делать по функции на прибор
+  public void leaveFastServer() {
+    updateReward();
+    if (fastServerDemand != null) {
+      fastServerDemand.setLeavingTime(context.getCurrentTime());
+      logValue(SOJOURN_TIME, fastServerDemand.getLeavingTime() - fastServerDemand.getArrivalTime());
+      fastServerDemand = null;
+      performAction(this::startFastService);
+    }
+  }
+
+  public void leaveSlowServer() {
+    updateReward();
+    if (slowServerDemand != null) {
+      slowServerDemand.setLeavingTime(context.getCurrentTime());
+      logValue(SOJOURN_TIME, slowServerDemand.getLeavingTime() - slowServerDemand.getArrivalTime());
+      slowServerDemand = null;
+      performAction(this::startSlowServer);
+    }
+  }
+
+
+  public void startFastService() {
+    updateReward();
+    if (queue.size() > 0 && fastServerDemand == null) {
+      fastServerDemand = queue.remove();
+      fastServerDemand.setServiceStartTime(context.getCurrentTime());
+      performActionAfterTimeout(this::leaveFastServer, fastServerRV.nextValue());
+    }
+  }
+
+
+  public void startSlowServer() {
+    updateReward();
+    if (queue.size() > 0 && slowServerDemand == null) {
+      double reward =
+          accumulatedRewardBetweenMakingDecisions;
+      lastTimeDecision = context.getCurrentTime();
+
+      int oldState = stateBeforeLastAction;
+      int currentState = getCurrentState();
+      updateQTable(oldState, lastAction, reward, currentState);
+
+      totalReward += accumulatedRewardBetweenMakingDecisions;
+      accumulatedRewardBetweenMakingDecisions = 0;
+
+      // Table was updated, make new decision and save it
+      int action = makeDecision(currentState);
+      lastAction = action;
+      stateBeforeLastAction = currentState;
+
+      trackRewardRate(totalReward / context.getCurrentTime());
+
+      if (action == 0) {
+        slowServerDemand = queue.remove();
+        slowServerDemand.setServiceStartTime(context.getCurrentTime());
+        performActionAfterTimeout(this::leaveSlowServer, slowServerRV.nextValue());
+      }
+    }
+  }
+
+
+  @Override
+  public void startService() {
+    if (queue.size() > 0) {
+      performAction(this::startFastService);
+      performAction(this::startSlowServer);
+    }
+  }
+
+  @Override
+  public void endService() {
+    //  оно не вызывается
+    System.out.println("endService");
+  }
+
+  @Override
+  public void receive(Parcel parcel) {
+    queue.add((Demand) parcel);
+    performAction(this::startService);
+  }
+
+
+  private int getNumberDemandInSystem() {
+    int processingDemandNumber = 0;
+    if (fastServerDemand != null) {
+      processingDemandNumber++;
+    }
+    if (slowServerDemand != null) {
+      processingDemandNumber++;
+    }
+    return queue.size() + processingDemandNumber;
+  }
+
+  private void initQTable() {
+    qTable = new double[stateNumber][ACTION_NUMBER];
+  }
+
+  private void updateQTable(int state, int action, double reward, int newState) {
+    double qCurrentValue = qTable[state][action];
+    double qTargetValue = reward + gamma * max(qTable[newState]);
+    qTable[state][action] += alpha * (qTargetValue - qCurrentValue);
+  }
+
+  private int makeDecision(int state) {
+    if (r.nextDouble() < epsilon || context.getCurrentTime() < warmUpDuration) {
+      return r.nextInt(ACTION_NUMBER);
+    } else {
+      return argMax(qTable[state]);
+    }
+  }
+
+  private double max(double[] d) {
+    return findMaximumElementAndItsIndex(d)[0];
+  }
+
+  private int argMax(double[] d) {
+    return (int) findMaximumElementAndItsIndex(d)[1];
+  }
+
+  private double[] findMaximumElementAndItsIndex(double[] d) {
+    double[] maxElementAndIndex = new double[2];
+    double maximum = Arrays.stream(d).max().getAsDouble();
+    int maxArg = ArrayUtils.indexOf(d, maximum);
+    maxElementAndIndex[0] = maximum;
+    maxElementAndIndex[1] = maxArg;
+    return maxElementAndIndex;
+  }
+
+  public void printQTable() {
+    for (int i = 0; i < stateNumber; i++) {
+      System.out.print(i + ":\t");
+      for (int j = 0; j < ACTION_NUMBER; j++) {
+        System.out.print(qTable[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+
+  public ArrayList<Integer> getPolicy() {
+    var result = new ArrayList<Integer>();
+    for (int i = 1; i < stateNumber; i++) {
+      result.add(argMax(qTable[i]));
+    }
+    return result;
+  }
+
+}
