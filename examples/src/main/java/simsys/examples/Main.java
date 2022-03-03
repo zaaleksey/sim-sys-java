7fdad9177086ef6254c94ca1048c2dee2e9be30f
Aleksey Zavarzin <alex.zavr.27100@gmail.com>
2022-03-03 17:02:08 +0400

    ckeck connections with github

---
+++ examples/src/main/java/simsys/examples/Main.java
@@ -0,0 +1,10 @@
+package simsys.examples;
+
+// this class is created to check ssh connections GitHub
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main check");
+    }
+
+}
